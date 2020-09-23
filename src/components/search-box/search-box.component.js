a340f386124601d0cb891df4d49f99177ef5274d
prociek <prociek@poczta.onet.pl>
2020-09-23 23:41:50 +0200

    added card-list card  search-box components

---
+++ src/components/search-box/search-box.component.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange}) => (
+    <input  class='search'
+            type='search' 
+            placeholder={placeholder} 
+            onChange={handleChange} />
+)
